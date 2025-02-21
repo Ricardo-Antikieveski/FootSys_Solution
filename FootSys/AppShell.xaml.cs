8bf995a0c81c3d0c1678b8ec8209f04a49dcf4f1
antik <antik@RICARDO-PC>
2025-02-20 22:00:43 -0300

    Adicionar arquivos de projeto.

---
+++ FootSys/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FootSys
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FootSys/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FootSys
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
